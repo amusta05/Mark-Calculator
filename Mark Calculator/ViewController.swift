4e8a1cf1427967b0efce91a8b5e396ed697a4a04
Areeb <amusta05@mail.uoguelph.ca>
2020-04-06 19:35:40 -0400

    Initial Commit

---
+++ Mark Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mark Calculator
+//
+//  Created by Areeb Mustafa on 2020-04-06.
+//  Copyright © 2020 Areeb Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
