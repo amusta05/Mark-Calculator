4d47f4a35cd4068825293c1b5c61d8bfdcbb521e
Areeb <amusta05@mail.uoguelph.ca>
2020-04-07 01:59:15 -0400

    back button working for calculate view controller

--- Mark Calculator/CalculateGradeViewController.swift
+++ Mark Calculator/CalculateGradeViewController.swift
@@ -14,17 +14,12 @@ class CalculateGradeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  
+    @IBAction func backButtonCalcPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
